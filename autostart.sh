f6dd577006c295416a8f7974bc7405fca3610bd3
Vladislav Li <couper64@gmail.com>
2025-01-19 14:14:25 +0000

    Init commit.

---
+++ autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ngrok http http://localhost:8000 > /dev/null &
+export WEBHOOK_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")"
+echo $WEBHOOK_URL
+curl -X PUT "https://kuil.pythonanywhere.com/update-ngrok-url/" -H "Content-Type: application/json" -d "{\"url\": $WEBHOOK_URL}"
