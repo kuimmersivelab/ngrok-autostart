6a3603ffa4402cb06400183c59d18f121b1efd22
Vladislav Li <couper64@gmail.com>
2025-01-23 15:55:27 +0000

    Bug fixing.

--- sub1-post-startup.sh
+++ sub1-post-startup.sh
@@ -3,5 +3,5 @@
 sleep 3
 export WEBHOOK_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")"
 echo $WEBHOOK_URL
-curl -X PUT "https://kuil.pythonanywhere.com/sub1/update-sub1-url/" -H "Content-Type: application/json" -d "{\"url\": $WEBHOOK_URL}"
+curl -X PUT "https://kuil.pythonanywhere.com/update-sub1-url/" -H "Content-Type: application/json" -d "{\"url\": $WEBHOOK_URL}"
 
