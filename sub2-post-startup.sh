0c9716f243b1aa6fca67797bd4e3d92a8eb822f6
Vladislav Li <couper64@gmail.com>
2025-01-23 16:18:59 +0000

    Added the last version that worked with a single domain.

--- sub2-post-startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 3
-export WEBHOOK_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[1].public_url")"
-echo $WEBHOOK_URL
-curl -X PUT "https://kuil.pythonanywhere.com/update-sub2-url/" -H "Content-Type: application/json" -d "{\"url\": $WEBHOOK_URL}"
-
--- sub2-startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ngrok http http://localhost:9000
-
