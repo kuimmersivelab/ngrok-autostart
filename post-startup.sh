76282d167b6246fc0cbb9bff6e0db059a1380030
Vladislav Li <couper64@gmail.com>
2025-01-24 16:48:57 +0000

    Added autoconfiguration of the local reverse-proxy.

--- post-startup.sh
+++ post-startup.sh
@@ -4,4 +4,6 @@ sleep 3
 export WEBHOOK_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")"
 echo $WEBHOOK_URL
 curl -X PUT "https://kuil.pythonanywhere.com/update-ngrok-url/" -H "Content-Type: application/json" -d "{\"url\": $WEBHOOK_URL}"
+curl -X PUT "http://localhost:6000/update-sub1-url/" -H "Content-Type: application/json" -d "{\"url\": \"http://talon_fsl:8000\"}"
+curl -X PUT "http://localhost:6000/update-sub2-url/" -H "Content-Type: application/json" -d "{\"url\": \"http://subserver:7000\"}"
 
